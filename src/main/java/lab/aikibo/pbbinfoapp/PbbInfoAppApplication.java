dcef0de438cbcac7a93e261c93534c8bb5f33f08
tamami <tamami.oka@gmail.com>
2018-01-01 13:57:50 +0700

    init commit

---
+++ src/main/java/lab/aikibo/pbbinfoapp/PbbInfoAppApplication.java
@@ -0,0 +1,13 @@
+package lab.aikibo.pbbinfoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbbInfoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbbInfoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lab/aikibo/pbbinfoapp/PbbInfoAppApplicationTests.java
@@ -0,0 +1,16 @@
+package lab.aikibo.pbbinfoapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PbbInfoAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
